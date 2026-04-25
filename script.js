b9615708b1ff2a3c8312e1131a25dd345fd35ea0
malaikaahsan <amz.va.malaika@gmail.com>
2026-04-25 21:59:31 +0500

    Final Change

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-import Card from '/card.js'
-
-
-console.log(Card.personalVariable)
-
 class memorygame {
     score = 0;
     container = [];
@@ -10,36 +5,38 @@ class memorygame {
     selectedcard = [];
     moves = 0;
     maxmoves = 10;
-    cardClass = new Card.Card()
+    
     constructor(cards, container) {
-        this.originalHTML = container.innerHTML;
         this.cards = cards;
         this.container = container;
-        this.startgame()
-        cards.forEach((card) => {
+        
+        
+        this.startgame();
+        
+      
+        this.cards.forEach((card) => {
             card.addEventListener('click', () => {
                 this.checkformatch(card);
-            })
-        })
-
-        // window.alert('helo')
-        console.log(this.cardClass.rendercard())
-
+            });
+        });
     }
+
     startgame() {
+       
         for (let i = this.cards.length - 1; i > 0; i--) {
             let j = Math.floor(Math.random() * (i + 1));
-            [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]]
-
+            [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
         }
-        this.container.innerHTML = " ";
+        
+        this.container.innerHTML = ""; 
         this.cards.forEach((card) => {
-            this.container.appendChild(card)
-        })
+            this.container.appendChild(card); 
+        });
     }
 
     checkformatch(card) {
-        if (this.selectedcard.length > 1 || card.classList.contains("flipped")) {
+       
+        if (this.selectedcard.length >= 2 || card.classList.contains("flipped")) {
             return;
         }
 
@@ -50,61 +47,85 @@ class memorygame {
             this.disableallcards();
             this.moves++;
             document.getElementById("moves").innerText = this.moves;
+            
             let type1 = this.selectedcard[0].querySelector(".card-inner").classList[1];
             let type2 = this.selectedcard[1].querySelector(".card-inner").classList[1];
+            
             if (type1 === type2) {
+               
                 this.score++;
-                document.getElementById("score").innerHTML = "score: " + this.score + " | moves: <span id='moves'>" + this.moves + "</span>";
+                document.getElementById("score").innerHTML = `score: ${this.score} | Moves: <span id='moves'>${this.moves}</span>`;
+                
                 setTimeout(() => {
                     this.selectedcard = [];
-                    this.cards.forEach(card => card.style.pointerEvents = "");
+                    this.cards.forEach(card => {
+                        
+                        if (!card.classList.contains("flipped")) {
+                            card.style.pointerEvents = "";
+                        }
+                    });
+                    
                     if (this.score === this.cards.length / 2) {
-                        setTimeout(() => {
-                            alert('Wins game');
-                        }, 500);
+                        setTimeout(() => alert('You win the game!'), 300);
                     }
                 }, 500);
+                
             } else {
+                
                 setTimeout(() => {
                     this.selectedcard[0].classList.remove("flipped");
                     this.selectedcard[1].classList.remove("flipped");
                     this.selectedcard = [];
-                    this.cards.forEach(card => card.style.pointerEvents = "");
+                    this.cards.forEach(card => {
+                        if (!card.classList.contains("flipped")) {
+                            card.style.pointerEvents = "";
+                        }
+                    });
                 }, 1000);
             }
+            
+         
             if (this.moves >= this.maxmoves && this.score < this.cards.length / 2) {
                 setTimeout(() => {
-                    alert('you lose the game | out of moves');
+                    alert('You lose the game | Out of moves');
                     this.disableallcards();
                 }, 1000);
             }
         }
     }
+
     disableallcards() {
         this.cards.forEach((card) => {
             card.style.pointerEvents = "none";
-        })
+        });
     }
+
     restartgame() {
-        this.container.innerHTML = this.originalHTML;
-        this.cards = Array.from(document.getElementsByClassName("card"));
+     
         this.selectedcard = [];
         this.moves = 0;
         this.score = 0;
-        this.cards.forEach(card => card.classList.remove("flipped"));
-        this.startgame();
-        this.cards.forEach((card) => {
-            card.replaceWith(card.cloneNode(true));
+        document.getElementById("score").innerHTML = `score: 0 | Moves: <span id='moves'>0</span>`;
+        
+     
+        this.cards.forEach(card => {
+            card.classList.remove("flipped");
+            card.style.pointerEvents = "";
         });
-        this.cards = Array.from(document.getElementsByClassName("card"));
-        this.cards.forEach((card) => {
-            card.addEventListener('click', () => {
-                this.checkformatch(card);
-            });
-        });
-        document.getElementById("score").innerHTML = "score: " + this.score + " | moves: <span id='moves'>" + this.moves + "</span>";
+
+        
+        setTimeout(() => {
+            this.startgame();
+        }, 800);
     }
 }
+
+
 let cards = Array.from(document.getElementsByClassName("card"));
 let container = document.querySelector(".container");
 let game = new memorygame(cards, container);
+
+
+document.getElementById("restart").addEventListener("click", () => {
+    game.restartgame();
+});
